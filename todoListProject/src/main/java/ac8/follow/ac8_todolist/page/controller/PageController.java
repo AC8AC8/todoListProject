abbd6a8d46c3be5374a7c920e52922f440154158
JangJaeHack(Jay) <myride.jay@gmail.com>
2022-12-01 14:27:18 +0900

    FEAT: PageController 작성

---
+++ todoListProject/src/main/java/ac8/follow/ac8_todolist/page/controller/PageController.java
@@ -0,0 +1,14 @@
+package ac8.follow.ac8_todolist.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "main";
+    }
+
+}
