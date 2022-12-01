8e714f0a9af1164ac3ff73bf8cef22034e241ce9
Yun-jong Lee <99802576+yjsayya@users.noreply.github.com>
2022-12-01 14:23:25 +0900

    Add files via upload

---
+++ todoListProject/src/main/java/ac8/follow/ac8_todolist/Ac8TodoListApplication.java
@@ -0,0 +1,13 @@
+package ac8.follow.ac8_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac8TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ac8TodoListApplication.class, args);
+    }
+
+}
---
+++ todoListProject/src/test/java/ac8/follow/ac8_todolist/Ac8TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ac8.follow.ac8_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac8TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
